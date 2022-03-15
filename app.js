513a7cf072188c8a32e7f0cda49e5fc5726f8978
Tedik <tamureylem@gmail.com>
2022-03-15 23:12:52 +0100

    route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express=require('express');
 const path = require('path');
 const mongoose= require('mongoose');
+const path = require ('path');
+const helmet=require('helmet')
 const password=process.env.PASSWORD
 const userName=process.env.USERNAME
 console.log({userName,password})
@@ -11,4 +13,17 @@ mongoose
 .then(()=>console.log("connecté à mangodb"))
 .catch ((err)=> console.error(err))
 const app=express();
+// definition de headers afin d'eviter les erreurs de Cors
+app.use ((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-header','Origin,X-Requested-With,Content,Accept,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTION');
+    next();
+});
+app.use(bodyParser.json());
+// Enregistrement des routeurs
+
+app.use('/api/sauce',sauceRoutes);
+app.use('/api/auth',userRoutes);
+
 module.exports = app;
--- models/sauce.js
+++ models/sauce.js
@@ -2,9 +2,16 @@
 const mongoose = require("mongoose");
 // schema
 const sauceSchema=mongoose.Schema({
-    userID:{type:String,require:true },
-    name:{ type:String,require:true},
-    imageUrl:{ type:String,require:true},
-    description:{type:String,require:true},
+    userID:{type:String,required:true },
+    name:{ type:String,required:true},
+    imageUrl:{ type:String,required:true},
+    description:{type:String,required:true},
+    mainPepper:{type:String,required:true},
+    imageUrl:{type:String,required:true},
+    heat:{type:Number,required:true},
+    likes:{type:Number,required:true,default: 0},
+    Userliked:{type:Number,required:true,default: 0},
+    userDisLiked:{type:Array,required:true,default:[]},
+    
 });
 module.exports=mongoose.model('sauce',sauceSchema);
